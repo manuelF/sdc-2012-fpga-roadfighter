4d684ecb859f65b3342c075925ebfd540f2daa99
jpdarago@gmail.com <jpdarago@gmail.com@57a015b4-6853-007f-738b-b4cc3b804826>
2012-08-29 20:34:30 +0000

    Ahora hay score (esta mal posicionado, las letras se ven medio mal, y cuenta siempre, pero llevo dos dias con esto, me voy a tomar un break)

--- RR/letter_memory.v
+++ RR/letter_memory.v
@@ -1,137 +1,112 @@
 
-// 5x5 sprite memory,
+// 8x7 sprite memory
 module letter_memory(
-
 	input [3:0] xcoord, ycoord,
-	input [4:0] letra,
-	input reset, pclk,
-//	input clk, Reset, pclk,	
-	output Rx, Gx, Bx
+	input [3:0] letra,
+	input reset,
+	output Rx, Gx, Bx, on
 	);
 	
+	localparam ROWS = 7;
+	localparam WIDTH = 8;
+	localparam PIXEL_WIDTH = WIDTH*3;
 	
-	localparam ROWS = 5;
-	localparam WIDTH = 5*3;
-	reg [WIDTH-1:0] letrarow;
+	reg [PIXEL_WIDTH-1:0] letra_mem [9:0] [ROWS-1:0];
 	
-	
-	reg [WIDTH-1:0] letra0[ROWS-1:0];
-	reg [WIDTH-1:0] letra1[ROWS-1:0];
-	reg [WIDTH-1:0] letra2[ROWS-1:0];
-	reg [WIDTH-1:0] letra3[ROWS-1:0];
-	reg [WIDTH-1:0] letra4[ROWS-1:0];
-	reg [WIDTH-1:0] letra5[ROWS-1:0];
-	reg [WIDTH-1:0] letra6[ROWS-1:0];
-	reg [WIDTH-1:0] letra7[ROWS-1:0];
-	reg [WIDTH-1:0] letra8[ROWS-1:0];
-	reg [WIDTH-1:0] letra9[ROWS-1:0];
-	
-	
-	// Initialize
-	always @(posedge reset) 
-	begin
-			// Letra 0
-			letra0[0] = 15'b111000000000111;
-			letra0[1] = 15'b000111111111000;
-			letra0[2] = 15'b000111111111000;
-			letra0[3] = 15'b000111111111000;
-			letra0[4] = 15'b111000000000111;
+	always @(posedge reset) begin
+		letra_mem[0][0] <= 24'b000111111111111111000000;
+		letra_mem[0][1] <= 24'b111000000000000000111000;
+		letra_mem[0][2] <= 24'b111000000000000000111000;
+		letra_mem[0][3] <= 24'b111000000000000000111000;
+		letra_mem[0][4] <= 24'b111000000000000000111000;
+		letra_mem[0][5] <= 24'b111000000000000000111000;
+		letra_mem[0][6] <= 24'b000111111111111111000000;
 
-			// Letra 1
-			letra1[0] = 15'b111000000111111;
-			letra1[1] = 15'b111111000111111;
-			letra1[2] = 15'b111111000111111;
-			letra1[3] = 15'b111111000111111;
-			letra1[4] = 15'b111000000000111;
+		// Letra 1
+		letra_mem[1][0] <= 24'b000111111111111000000000;
+		letra_mem[1][1] <= 24'b000000000111111000000000;
+		letra_mem[1][2] <= 24'b000000000111111000000000;
+		letra_mem[1][3] <= 24'b000000000111111000000000;
+		letra_mem[1][4] <= 24'b000000000111111000000000;
+		letra_mem[1][5] <= 24'b000000000111111000000000;
+		letra_mem[1][6] <= 24'b000111111111111111000000;
 
-			// Letra 2
-			letra2[0] = 15'b111000000000111;
-			letra2[1] = 15'b111111111000111;
-			letra2[2] = 15'b111000000000111;
-			letra2[3] = 15'b111000111111111;
-			letra2[4] = 15'b111000000000111;
+		// Letra 2
+		letra_mem[2][0] <= 24'b000111111111111111000000;
+		letra_mem[2][1] <= 24'b000000000000111111000000;
+		letra_mem[2][2] <= 24'b000000000000111111000000;
+		letra_mem[2][3] <= 24'b000111111111111111000000;
+		letra_mem[2][4] <= 24'b000111111000000000000000;
+		letra_mem[2][5] <= 24'b000111111000000000000000;
+		letra_mem[2][6] <= 24'b000111111111111111000000;
 
-			// Letra 3
-			letra3[0] = 15'b111000000000111;
-			letra3[1] = 15'b111111111000111;
-			letra3[2] = 15'b111000000000111;
-			letra3[3] = 15'b111111111000111;
-			letra3[4] = 15'b111000000000111;
+		// Letra 3
+		letra_mem[3][0] <= 24'b000111111111111111000000;
+		letra_mem[3][1] <= 24'b000000000000111111000000;
+		letra_mem[3][2] <= 24'b000000000000111111000000;
+		letra_mem[3][3] <= 24'b000111111111111111000000;
+		letra_mem[3][4] <= 24'b000000000000111111000000;
+		letra_mem[3][5] <= 24'b000000000000111111000000;
+		letra_mem[3][6] <= 24'b000111111111111111000000;
+		
+		// Letra 4
+		letra_mem[4][0] <= 24'b000000000111111111000000;
+		letra_mem[4][1] <= 24'b000000111111111111000000;
+		letra_mem[4][2] <= 24'b000111111000111111000000;
+		letra_mem[4][3] <= 24'b111111000000111111000000;
+		letra_mem[4][4] <= 24'b111111111111111111000000;
+		letra_mem[4][5] <= 24'b000000000000111111000000;
+		letra_mem[4][6] <= 24'b000000000000111111000000;
 
-			// Letra 4
-			letra4[0] = 15'b111111000000111;
-			letra4[1] = 15'b111000111000111;
-			letra4[2] = 15'b000000000000111;
-			letra4[3] = 15'b111000111000111;
-			letra4[4] = 15'b111000111000111;
+		// Letra 5
+		letra_mem[5][0] <= 24'b111111111111111111111000;
+		letra_mem[5][1] <= 24'b111000000000000000000000;
+		letra_mem[5][2] <= 24'b111000000000000000000000;
+		letra_mem[5][3] <= 24'b111111111111111111111000;
+		letra_mem[5][4] <= 24'b000000000000000000111000;
+		letra_mem[5][5] <= 24'b000000000000000000111000;
+		letra_mem[5][6] <= 24'b111111111111111111111000;
 
-			// Letra 5
-			letra5[0] = 15'b111000000000111;
-			letra5[1] = 15'b111000111111111;
-			letra5[2] = 15'b111000000000111;
-			letra5[3] = 15'b111000111000111;
-			letra5[4] = 15'b111000000000111;
+		// Letra 6
+		letra_mem[6][0] <= 24'b000111111111111111000000;
+		letra_mem[6][1] <= 24'b111000000000000000000000;
+		letra_mem[6][2] <= 24'b111000000000000000000000;
+		letra_mem[6][3] <= 24'b111111111111111111000000;
+		letra_mem[6][4] <= 24'b111000000000000000111000;
+		letra_mem[6][5] <= 24'b111000000000000000111000;
+		letra_mem[6][6] <= 24'b000111111111111111000000;
 
-			// Letra 6
-			letra6[0] = 15'b111000000000111;
-			letra6[1] = 15'b111000111000111;
-			letra6[2] = 15'b111000000000111;
-			letra6[3] = 15'b111000111000111;
-			letra6[4] = 15'b111000000000111;
+		// Letra 7
+		letra_mem[7][0] <= 24'b111111111111111111111000;
+		letra_mem[7][1] <= 24'b000000000000000111111000;
+		letra_mem[7][2] <= 24'b000000000000111111000000;
+		letra_mem[7][3] <= 24'b000000000111111000000000;
+		letra_mem[7][4] <= 24'b000000111111000000000000;
+		letra_mem[7][5] <= 24'b000111111000000000000000;
+		letra_mem[7][6] <= 24'b111111000000000000000000;
 
-			// Letra 7
-			letra7[0] = 15'b111000000000111;
-			letra7[1] = 15'b111000111000111;
-			letra7[2] = 15'b111111111000111;
-			letra7[3] = 15'b111111111000111;
-			letra7[4] = 15'b111111111000111;
+		// Letra 8
+		letra_mem[8][0] <= 24'b000111111111111111000000;
+		letra_mem[8][1] <= 24'b111000000000000000111000;
+		letra_mem[8][2] <= 24'b111000000000000000111000;
+		letra_mem[8][3] <= 24'b000111111111111111000000;
+		letra_mem[8][4] <= 24'b111000000000000000111000;
+		letra_mem[8][5] <= 24'b111000000000000000111000;
+		letra_mem[8][6] <= 24'b000111111111111111000000;
 
-			// Letra 8
-			letra8[0] = 15'b111000000000111;
-			letra8[1] = 15'b111000111000111;
-			letra8[2] = 15'b111000000000111;
-			letra8[3] = 15'b111000111000111;
-			letra8[4] = 15'b111000000000111;
-
-			// Letra 9
-			letra9[0] = 15'b111000000000111;
-			letra9[1] = 15'b111000111000111;
-			letra9[2] = 15'b111000000000111;
-			letra9[3] = 15'b111111111000111;
-			letra9[4] = 15'b111000000000111;		
+		// Letra 9
+		letra_mem[9][0] <= 24'b000111111111111111000000;
+		letra_mem[9][1] <= 24'b111000000000000000111000;
+		letra_mem[9][2] <= 24'b111000000000000000111000;
+		letra_mem[9][3] <= 24'b000111111111111111111000;
+		letra_mem[9][4] <= 24'b000000000000000000111000;
+		letra_mem[9][5] <= 24'b000000000000000000111000;
+		letra_mem[9][6] <= 24'b000111111111111111000000;	
 	end
-	
-	// Assign signals to proper outputs
-	reg R,G,B;
-	always @(posedge pclk) 
-	begin
 
-		if (letra == 0)
-			letrarow = letra0[ycoord];
-		if (letra == 1)
-			letrarow = letra1[ycoord];
-		if (letra == 2)
-			letrarow = letra2[ycoord];
-		if (letra == 3)
-			letrarow = letra3[ycoord];
-		if (letra == 4)
-			letrarow = letra4[ycoord];
-		if (letra == 5)
-			letrarow = letra5[ycoord];
-		if (letra == 6)
-			letrarow = letra6[ycoord];
-		if (letra == 7)
-			letrarow = letra7[ycoord];
-		if (letra == 8)
-			letrarow = letra8[ycoord];
-		if (letra == 9)
-			letrarow = letra9[ycoord];
-	
-		R = letrarow[(xcoord*3)+2];
-		G = letrarow[(xcoord*3)+1];
-		B = letrarow[(xcoord*3)+0];		
-	end
-	assign Rx=R;
-	assign Gx=G;
-	assign Bx=B;
+	assign Rx = letra_mem[letra][ycoord][3*(WIDTH-1-xcoord)+2];
+	assign Gx = letra_mem[letra][ycoord][3*(WIDTH-1-xcoord)+1];
+	assign Bx = letra_mem[letra][ycoord][3*(WIDTH-1-xcoord)+0];
+	assign on = Rx == 1 && Gx == 1 && Bx == 1;
+
 endmodule
