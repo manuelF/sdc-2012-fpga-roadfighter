1b19f06f7302724e935dc142d212e4becfdd7f55
jpdarago <jpdarago@57a015b4-6853-007f-738b-b4cc3b804826>
2013-07-30 00:00:25 +0000

    bajo completo

---
+++ trunk/music/parse_xml.py
@@ -0,0 +1,42 @@
+from xml.dom.minidom import parse
+import fpga2pysynth
+
+durations = {
+	"16th": 1,
+	"eighth": 2,
+	"quarter": 4
+}
+
+def get(n,k): 
+	v = n.getElementsByTagName(k)
+	t = v[0].tagName if v else None
+	if not t: return None
+	return v[0].firstChild.data if v[0].firstChild else t
+
+def get_note_data(node):
+	note = get(node,"step") or ""
+	alter = "#" if get(node,"alter") else ""
+
+	octave = get(node,"octave")
+	octave = str(int(octave)+1) if octave else "r"
+
+	duration = durations[get(node,"type")]
+	dot = get(node,"dot")
+	if dot: duration += duration/2
+	
+	res = ""
+	while duration > 0:
+		res += str(fpga2pysynth.encode_note(note.lower() + alter + octave)) + ",\n"
+		duration -= 1
+	return res
+
+dom = parse("onlybass.xml")
+notes = dom.getElementsByTagName("note")
+
+res = ""
+res += "memory_initialization_radix=10;\n"
+res += "memory_initialization_vector=\n"
+
+for note in notes[1:]:
+	res += get_note_data(note)
+print res[:-2] + ";"
